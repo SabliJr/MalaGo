719b918c4ad8457493d1ca30e6540680a9511fbe
Sabli Jr <sablijr@yahoo.com>
2023-04-19 12:14:53 +0200

    Adjust the home page

--- src/Components/Destinations/Activities.tsx
+++ src/Components/Destinations/Activities.tsx
@@ -1,15 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Activities.css";
-
-//Images
-import BlueLagoon from "../../Assets/Blue-Lagoon.webp";
-import Culture from "../../Assets/Culture.jpg";
-import Diving from "../../Assets/Diving.jpeg";
-import Festival from "../../Assets/Festival.jpeg";
-import Mellieha from "../../Assets/Mellieha.webp";
-import Climbing from "../../Assets/Climbing.webp";
-import Cruises from "../../Assets/Malta.jpeg";
-import WaterSports from "../../Assets/Kayaking.jpeg";
+import ActivitiesData from "../../Data/Activities";
 
 //Icons
 import { RiArrowLeftLine, RiArrowRightLine } from "react-icons/ri";
@@ -17,47 +8,39 @@ import { RiArrowLeftLine, RiArrowRightLine } from "react-icons/ri";
 interface IActivities {}
 
 const Activities: React.FC<IActivities> = (props) => {
+  const [start, setStart] = useState<number>(1);
+
   return (
     <section className='activitiesSec'>
       <div className='ArrowDiv'>
         <h2 className='activities'>Activities</h2>
       </div>
-      <RiArrowLeftLine className='ArrowIcon1' />
+      {start === 1 ? null : (
+        <RiArrowLeftLine
+          className='ArrowIcon1'
+          onClick={() => setStart(start - 1)}
+        />
+      )}
+
       <article className='activitiesArt'>
-        <div>
-          <img src={BlueLagoon} alt='BlueLagoon' className='activitiesImg' />
-          <h4>Beaches</h4>
-        </div>
-        <div>
-          <img src={Mellieha} alt='Mellieha' className='activitiesImg' />
-          <h4>Art & History</h4>
-        </div>
-        <div>
-          <img src={Diving} alt='Diving' className='activitiesImg' />
-          <h4>Diving</h4>
-        </div>
-        <div>
-          <img src={Climbing} alt='Climbing' className='activitiesImg' />
-          <h4>Climbing</h4>
-        </div>
-        <div>
-          <img src={Cruises} alt='Cruises' className='activitiesImg' />
-          <h4>Cruises</h4>
-        </div>
-        <div>
-          <img src={Culture} alt='Culture' className='activitiesImg' />
-          <h4>Culture</h4>
-        </div>
-        <div>
-          <img src={Festival} alt='Festival' className='activitiesImg' />
-          <h4>Religues Festival</h4>
-        </div>
-        <div>
-          <img src={WaterSports} alt='WaterSports' className='activitiesImg' />
-          <h4>Water Sports</h4>
-        </div>
+        {ActivitiesData.map((activity) => (
+          <div key={activity.id}>
+            <img
+              src={activity.Image}
+              alt='ActivityImage'
+              className='activityImg'
+            />
+            <h4>{activity.title}</h4>
+          </div>
+        ))}
       </article>
-      <RiArrowRightLine className='ArrowIcon2' />
+
+      {start === -1 ? null : (
+        <RiArrowRightLine
+          className='ArrowIcon2'
+          onClick={() => setStart(start + 1)}
+        />
+      )}
     </section>
   );
 };
---
+++ src/Data/Activities.ts
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const Active = [
+    {
+        id: uuidv4(),
+        Image: "../Images/Blue-Lagoon.webp",
+        title: 'Beaches'
+    },
+     {
+        id: uuidv4(),
+        Image: "../Images/Culture.jpg",
+        title: 'Art & History'
+    },
+      {
+        id: uuidv4(),
+        Image: "../Images/Diving.jpeg",
+        title: 'Diving'
+    },
+             {
+        id: uuidv4(),
+        Image: "../Images/Climbing.webp",
+        title: 'Climbing'
+    },
+       {
+        id: uuidv4(),
+        Image: "../Images/Festival.jpeg",
+        title: 'Cruises'
+    },
+        {
+        id: uuidv4(),
+        Image: "../Images/Mellieha.webp",
+        title: 'Culture'
+    },
+
+          {
+        id: uuidv4(),
+        Image: "../Images/Malta.jpeg",
+        title: 'Religious Festivals'
+    },
+           {
+        id:uuidv4(),
+        Image: "../Images/Kayaking.jpeg",
+        title: 'Water Sports'
+    },
+]
+
+export default Active
